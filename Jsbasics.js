d876000bdfefc68175c4326fdb9e7e14b244ac89
Boxrooll <tony.corliss@stu.barren.kyschools.us>
2025-03-10 10:17:57 -0500

    first commit

---
+++ Jsbasics.js
@@ -0,0 +1,61 @@
+// 1
+console.log(14);
+
+// 2
+// Numbers, Strings, Booleans, Null, & Undefined
+// NOTE: Symbols are also a primitive (AKA Data Type). However, they are NOT commonly used.
+
+// 3
+// Addition is +
+// Subtraction is -
+// Multiplication is *
+// Division is /
+// Exponentiation is **
+// Modulus is %
+
+// 4
+console.log(5 % 4); // 1
+console.log(5 % 2); // 1
+// console.log(5 % -4);
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL!'); // I'M MARY POPPINS, Y'ALL!
+
+// 6
+console.log("Hawk" + "eye"); // Hawkeye
+
+// 7
+console.log("Matthew".length + "Riley".length); // 9
+// console.log(4 + 5);
+// console.log(9);
+
+// 8
+console.log("Avengers: Endgame"[16]); // e
+
+// BONUS
+// 1
+console.log("\\"); // \
+
+// 2
+console.log(5 ** 4 - 9); // 616
+// console.log(625 - 9);
+// console.log(616);
+
+// 3
+console.log("I".length ** "Am".length - "Iron".length + "Man".length); // 0
+
+// 4
+console.log("CAP"[1]); // A
+console.log("VISION"[0]); // V
+console.log("OKOYE"[4]); // E
+console.log("NICK FURY"[0]); // N
+console.log("GROOT"[0]); // G
+console.log("DOCTOR STRANGE"[13]); // E
+console.log("THOR"[3]); // R
+console.log("STAR-LORD"[0]); // S
+
+// 5
+m = 25;
+c = 4;
+u = m + c;
+console.log(u); // 29
